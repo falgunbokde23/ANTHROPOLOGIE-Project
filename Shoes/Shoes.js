404d97aa0e1998bc1a55133fa086e30e9c9ba533
Aditya Singh Ghosh <adityasinghghosh36@gmail.com>
2022-05-07 00:32:21 +0530

    Shoes has been added

---
+++ Shoes/Shoes.js
@@ -0,0 +1,347 @@
+var WomensShoesData = [
+  {
+    image_url: "https://images.urbndata.com/is/image/Anthropologie/66430166_025_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Maeve Puffy Knotted Sandals ",
+    price: 80,
+    strikedoffprice: "$2599",
+  },
+  {
+    image_url: "https://images.urbndata.com/is/image/Anthropologie/66435637_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Birkenstock Glenda Sandals ",
+    price: 140,
+    strikedoffprice: 999,
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/66604497_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Jeffrey Campbell Linques Sandals ",
+    price: 130,
+    strikedoffprice: 2099,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/66174640_025_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Seychelles Low Key Glow Up Sandals",
+    price: 99,
+    strikedoffprice: 799,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/65565731_011_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: " Teva Hurricane Verge Sandals",
+    price: 80,
+    strikedoffprice: 3599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67107052_050_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Maeve Puffy Knotted Sandals ",
+    price: 80,
+    strikedoffprice: 3199,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/59967851_016_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Melissa Bikini Platform Sandals ",
+    price: 40,
+    strikedoffprice: 3199,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67106484_080_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Teva Hurricane Verge Sandals ",
+    price: 79,
+    strikedoffprice: 1099,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/65565319_024_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Seychelles Adapt Heel  ",
+    price: 23,
+    strikedoffprice: 1440,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/59969535_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: "Seychelles Adapt Heel",
+    price: 45,
+    strikedoffprice: 999,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/66540873_060_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Seychelles Adapt Heel ",
+    price: 32,
+    strikedoffprice: 3599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/66737909_070_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: "Seychelles Adapt Heel",
+    price: 34,
+    strikedoffprice: 3599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/63333678_015_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Bernardo Miami Sandals",
+    price: 76,
+    strikedoffprice: 999,
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/69434926_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Bernardo Miami Sandals",
+    price: 65,
+    strikedoffprice: 3499,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67059634_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: "Bernardo Miami Sandals ",
+    price: 43,
+    strikedoffprice: 12000,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/66706037_026_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: " Bernardo Miami Sandals ",
+    price: 54,
+    strikedoffprice: 1299,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/66938895_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: " Pilcro Tie-Up Thong Sandals ",
+    price: 1019,
+    strikedoffprice: 1699,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67555896_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: "Pilcro Tie-Up Thong Sandals  ",
+    price: 32,
+    strikedoffprice: 1699,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/68463264_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Pilcro Tie-Up Thong Sandals",
+    price: 35,
+    strikedoffprice: 3499,
+  },
+  {
+    image_url:
+      " https://images.urbndata.com/is/image/Anthropologie/68452002_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: "  Pilcro Tie-Up Thong Sandals ",
+    price: 65,
+    strikedoffprice: 4999,
+  },
+  {
+    image_url:
+      " https://images.urbndata.com/is/image/Anthropologie/66374158_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Pilcro Tie-Up Thong Sandals ",
+    price: 45,
+    strikedoffprice: 1499,
+  },
+  {
+    image_url:
+      " https://images.urbndata.com/is/image/Anthropologie/67058594_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " Pilcro Tie-Up Thong Sandals ",
+    price: 65,
+    strikedoffprice: 2299,
+  },
+  {
+    image_url:" https://images.urbndata.com/is/image/Anthropologie/67058594_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: " Pilcro Tie-Up Thong Sandals ",
+    price: 65,
+    strikedoffprice: 3599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67115014_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: "Birkenstock Siena Vegan Sandals",
+      price: 35,
+    strikedoffprice: 1299,
+  },
+  {
+    image_url:
+      " https://images.urbndata.com/is/image/Anthropologie/69433654_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: "Birkenstock Siena Vegan Sandals ",
+    price: 74,
+    strikedoffprice: 4999,
+  },
+  {
+    image_url:
+      " https://images.urbndata.com/is/image/Anthropologie/66737909_070_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349  ",
+    name: " Sarto Frita Heel ",
+    price: 32,
+    strikedoffprice: 4999,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/65566572_066_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349  ",
+    name: " Tave Original Sandals ",
+    price: 40,
+    strikedoffprice: 2799,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/68181247_066_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+    name: " SAm Jildie Heels",
+    price: 33,
+    strikedoffprice: 3499,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67353219_011_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Pilcro Woven Leather Flat",
+    price: 39,
+    strikedoffprice: 5599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/67553727_010_b14?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Pilcro Woven Leather Flat",
+    price: 54,
+    strikedoffprice: 3599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/68184431_055_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Jeffery Heels",
+    price: 440,
+    strikedoffprice: 3599,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/69430197_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Freedom Moses Bali Sandals",
+    price: 250,
+    strikedoffprice: 9999,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/68442698_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Apl Logo Slide Sandals",
+    price: 356,
+    strikedoffprice: 1299,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/62519178_006_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "Pilcro Woven Leather Flat",
+    price: 143,
+    strikedoffprice: 1299,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/66978487_015_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: " Freedom Moses DOts Sandals",
+    price: 235,
+    strikedoffprice: 2499,
+  },
+  {
+    image_url:
+      "https://images.urbndata.com/is/image/Anthropologie/69423507_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name: "UGG L.A. Sun Sandals",
+    price: 115,
+    strikedoffprice: 1799,
+  },
+];
+
+
+
+
+
+var basketLSData = JSON.parse(localStorage.getItem("basket")) || [];
+
+document.querySelector("#filter").addEventListener("change", handle_sort);
+
+function handle_sort(){
+  var sort_type = document.querySelector("#filter").value;
+  
+  if(sort_type=="none"){
+    Display(WomensShoesData);
+  }
+  else if(sort_type == "asc" ){
+    WomensShoesData.sort(function (a, b){
+      return a.price - b.price;
+    });
+    // console.log(Beauty_wellness);
+    Display(WomensShoesData);
+  }
+  else if(sort_type == "dec"){
+    WomensShoesData.sort(function (a,b){
+      return b.price - a.price;
+    });
+    Display(WomensShoesData);
+  }
+  else{
+    var filtered = WomensShoesData.filter(function (elem){
+      return elem.category == sort_type;
+    })
+    Display(filtered);
+  }
+}
+    // console.log(Beauty_wellness)
+
+    Display(WomensShoesData);
+
+function Display(WomensShoesData){
+
+    document.querySelector("#beauty_product_container").innerHTML ="";
+    WomensShoesData.forEach(function(elm){
+
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    img.style.width = "100%"
+    img.setAttribute("src", elm.image_url);
+    var Name = document.createElement("h3");
+    Name.innerText = elm.name;
+    var dollar = document.createElement("h4");
+    dollar.innerText = "$";
+    dollar.style.display = "inline"
+    var Price = document.createElement("h4");
+    Price.innerText = elm.price;
+    Price.style.display = "inline"
+  
+    var button = document.createElement("button");
+    button.innerText = "Add to basket";
+    button.style.display = "block"
+    button.setAttribute("id", "add_to_basket");
+    button.addEventListener("click", Addtobasket);
+    function Addtobasket(){
+      var addedProduct = {
+        image: elm.image_url,
+        name: elm.name,
+        price: elm.price
+      }
+      // console.log(elm.image_url);
+      basketLSData.push(addedProduct);
+      localStorage.setItem("basket", JSON.stringify(basketLSData));
+      alert("Product added Successfully");
+    }
+    document.querySelector("#beauty_product_container").append(div);
+    div.append(img, Name, dollar, Price, button);
+
+
+
+  });
+}
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
