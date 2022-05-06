7ca82354e93f22164fc0372a803a243511381a03
Aditya Singh Ghosh <adityasinghghosh36@gmail.com>
2022-05-07 00:28:55 +0530

    Garden has been added

---
+++ Garden/Garden.js
@@ -0,0 +1,247 @@
+
+  var GardenData = [
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/B45246403AA_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: "Peacock Ottoman",
+      price: 434.00,
+      strikedoffprice: 3599,
+    },
+    {
+      image_url:
+             "https://images.urbndata.com/is/image/Anthropologie/45246403AA_001_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: " Peacock indoor/Outdoor Daybed",
+      price: 976,
+      strikedoffprice: 999,
+    },
+    {
+      image_url:"https://images.urbndata.com/is/image/Anthropologie/56046469_041_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Plumage indoor/Outdoor Rattan Chair",
+      price: 965,
+      strikedoffprice: 3499,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/A45246403AA_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: "Peacock abana Daybed ",
+      price: 1143,
+      strikedoffprice: 12000,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/45549367-0000-SC0006_b?$a15-mto-tile$&hei=523&wid=349&qlt=80&fit=constrain",
+      name: " Barwick indoor/Outdoor Swivel Chair ",
+      price: 1354,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/49812944_006_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: " Ingrid Chair ",
+      price: 298,
+      strikedoffprice: 1699,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/66490830_018_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: " Sara Campbell picnic Blanket ",
+      price: 92,
+      strikedoffprice: 1699,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/66013038_049_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Celma Indoor/Outdoor Pillow",
+      price: 65,
+      strikedoffprice: 3499,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/67118968_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Pari Rattan Bench ",
+      price: 65,
+      strikedoffprice: 4999,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/66150368_010_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Outdoor Umbrella Stand ",
+      price: 245,
+      strikedoffprice: 1499,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/65847659_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Mosaic Stool ",
+      price: 165,
+      strikedoffprice: 2299,
+    },
+    {
+      image_url:"https://images.urbndata.com/is/image/Anthropologie/45217557AA_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Ianthe Indoor Pillow ",
+      price: 65,
+      strikedoffprice: 3599,
+    },
+    {
+      image_url:
+        "  https://images.urbndata.com/is/image/Anthropologie/50294149_042_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+        name: " Handwoven Harani",
+        price: 75,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/68956697_066_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Dried King PRtea",
+      price: 74,
+      strikedoffprice: 4999,
+    },
+    {
+      image_url:
+        "  https://images.urbndata.com/is/image/Anthropologie/64741697_030_b10?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349 ",
+      name: " Nadya Ribbed Watering Can ",
+      price: 32,
+      strikedoffprice: 4999,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/63692321_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: " Pincushion Flower Bunch ",
+      price: 80.,
+      strikedoffprice: 2799,
+    },
+    {
+      image_url:
+        " https://images.urbndata.com/is/image/Anthropologie/58361411_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: " Fresh Free Spirit Roses Dozen",
+      price: 133,
+      strikedoffprice: 3499,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/54503313_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: "Fresh Pampas Grass ",
+      price: 69,
+      strikedoffprice: 5599,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/57191066_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: "Fresh Blue + White Scabiosa Bunch",
+      price: 94,
+      strikedoffprice: 3599,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/64102718_030_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: "Thick Ridge Ceramic",
+      price: 40,
+      strikedoffprice: 3599,
+    },
+    {
+      image_url:
+        "https://images.urbndata.com/is/image/Anthropologie/63379903_014_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+      name: "Fresh Iris Bunch",
+      price: 115,
+      strikedoffprice: 1799,
+    },
+  ];
+
+ 
+
+
+
+
+var basketLSData = JSON.parse(localStorage.getItem("basket")) || [];
+
+document.querySelector("#filter").addEventListener("change", handle_sort);
+
+function handle_sort(){
+  var sort_type = document.querySelector("#filter").value;
+  
+  if(sort_type=="none"){
+    Display(GardenData);
+  }
+  else if(sort_type == "asc" ){
+    GardenData.sort(function (a, b){
+      return a.price - b.price;
+    });
+    // console.log(Beauty_wellness);
+    Display(GardenData);
+  }
+  else if(sort_type == "dec"){
+    GardenData.sort(function (a,b){
+      return b.price - a.price;
+    });
+    Display(GardenData);
+  }
+  else{
+    var filtered = GardenData.filter(function (elem){
+      return elem.category == sort_type;
+    })
+    Display(filtered);
+  }
+}
+    // console.log(Beauty_wellness)
+
+    Display(GardenData);
+
+function Display(GardenData){
+
+    document.querySelector("#beauty_product_container").innerHTML ="";
+    GardenData.forEach(function(elm){
+
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    img.style.width = "100%"
+    img.setAttribute("src", elm.image_url);
+    var Name = document.createElement("h3");
+    Name.innerText = elm.name;
+    var dollar = document.createElement("h4");
+    dollar.innerText = "$";
+    dollar.style.display = "inline"
+    var Price = document.createElement("h4");
+    Price.innerText = elm.price;
+    Price.style.display = "inline"
+  
+    var button = document.createElement("button");
+    button.innerText = "Add to basket";
+    button.style.display = "block"
+    button.setAttribute("id", "add_to_basket");
+    button.addEventListener("click", Addtobasket);
+    function Addtobasket(){
+      var addedProduct = {
+        image: elm.image_url,
+        name: elm.name,
+        price: elm.price
+      }
+      // console.log(elm.image_url);
+      basketLSData.push(addedProduct);
+      localStorage.setItem("basket", JSON.stringify(basketLSData));
+      alert("Product added Successfully");
+    }
+    document.querySelector("#beauty_product_container").append(div);
+    div.append(img, Name, dollar, Price, button);
+
+
+
+  });
+}
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
