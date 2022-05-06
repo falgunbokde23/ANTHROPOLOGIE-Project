3d4c313d18319e39c66b3e9927e4b55b24507565
asheesh jaat <asheeshjaat2175@gmail.com>
2022-05-07 00:19:32 +0530

    momgift has been added

---
+++ momgift/giftmom.js
@@ -0,0 +1,301 @@
+var giftmomDATA=[{
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/65653032_048_b15?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Nostalagia Candle",
+     doller:"$",
+     price:"24.00"
+    
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67542654_079_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Moxy-Collective Florl Pajama Pants",
+     doller:"$",
+     price:"58.00",
+    
+    
+  
+  },
+  
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/68087998_901_b10?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Pearl Monogram Bracelet Set",
+     doller:"$",
+     price:"38.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/37659125_901_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Monogram Pendant Necklace",
+     doller:"$",
+     price:"38.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67437731_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mommy & Me Daphne Floral Headband",
+     doller:"$",
+     price:"268.00",
+  
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67442533_041_b14?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mama Dish Towel",
+     doller:"$",
+     price:"20.00",
+  
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/66793282_102_b11?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Old Havana pie Dish",
+     doller:"$",
+     price:"44.00",
+  
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/47082029_083_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Jimene Occasional Chair",
+     doller:"$",
+     price:"998.00",
+  
+    
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/65534950_066_b10?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Sculpture Garden Candle",
+     doller:"$",
+     price:"44.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/66769589_049_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Dannijo Huggie Drp Earrings",
+     doller:"$",
+     price:"148.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/66769613_049_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Dannijo Francesca Heart-Shaped Drop Necklace",
+     doller:"$",
+     price:"228.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67437772_102_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mommy & Me Pearl Headband",
+     doller:"$",
+     price:"198.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/54102041_070_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Dapper Dog Wine Bottle Holder",
+     doller:"$",
+     price:"118.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/65780488_901_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Blocjprinted Monogram Mug",
+     doller:"$",
+     price:"12.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67437756_043_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mommy & Me Floral Beaded Headband",
+     doller:"$",
+     price:"268.00",
+   
+  
+  },
+  
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/64934409_048_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mer-Sea &Co.Avant Garden Bath Saok",
+     doller:"$",
+     price:"20.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/68687821_070_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Cloverport Ellie Neckle",
+     doller:"$",
+     price:"123.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67542654_079_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Moxy-Collective Florl Pajama Pants",
+     doller:"$",
+     price:"158.00",
+    
+    
+  
+  },
+  
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/68087998_901_b10?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Pearl Monogram Bracelet Set",
+     doller:"$",
+     price:"308.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/37659125_901_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Monogram Pendant Necklace",
+     doller:"$",
+     price:"380.00",
+   
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67437731_000_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mommy & Me Daphne Floral Headband",
+     doller:"$",
+     price:"28.00",
+  
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/67442533_041_b14?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Mama Dish Towel",
+     doller:"$",
+     price:"204.00",
+  
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/66793282_102_b11?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Old Havana pie Dish",
+     doller:"$",
+     price:"440.00",
+  
+  
+  },
+  {
+    image_url:"https://images.urbndata.com/is/image/Anthropologie/47082029_083_b?$an-category$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    name:"Jimene Occasional Chair",
+     doller:"$",
+     price:"98.00",
+  
+    
+  
+  },
+  
+  
+  
+  
+  
+  ]
+  
+  
+  
+  var basketLSData = JSON.parse(localStorage.getItem("basket")) || [];
+  
+  document.querySelector("#filter").addEventListener("change", handle_sort);
+  
+  function handle_sort(){
+    var sort_type = document.querySelector("#filter").value;
+    
+    if(sort_type=="none"){
+      Display(giftmomDATA);
+    }
+    else if(sort_type == "asc" ){
+      giftmomDATA.sort(function (a, b){
+        return a.price - b.price;
+      });
+      // console.log(giftmomDATA);
+      Display(giftmomDATA);
+    }
+    else if(sort_type == "dec"){
+      giftmomDATA.sort(function (a,b){
+        return b.price - a.price;
+      });
+      Display(giftmomDATA);
+    }
+    else{
+      var filtered = giftmomDATA.filter(function (elem){
+        return elem.category == sort_type;
+      })
+      Display(filtered);
+    }
+  }
+      // console.log(giftmomDATA)
+  
+      Display(giftmomDATA);
+  
+  function Display(giftmomDATA){
+  
+      document.querySelector("#beauty_product_container").innerHTML ="";
+      giftmomDATA.forEach(function(elm){
+  
+      var div = document.createElement("div");
+      var image_url = document.createElement("img");
+      image_url.style.width = "100%"
+      image_url.setAttribute("src", elm.image_url);
+      var Name = document.createElement("h3");
+      Name.innerText = elm.name;
+      var dollar = document.createElement("h4");
+      dollar.innerText = "$";
+      dollar.style.display = "inline"
+      var Price = document.createElement("h4");
+      Price.innerText = elm.price;
+      Price.style.display = "inline"
+      
+      var button = document.createElement("button");
+      button.innerText = "Add to basket";
+      button.style.display = "block"
+      button.setAttribute("id", "add_to_basket");
+      button.addEventListener("click", Addtobasket);
+      function Addtobasket(){
+        var addedProduct = {
+          image: elm.image_url,
+          name: elm.name,
+          price: elm.price
+        }
+        // console.log(elm.image_url);
+        basketLSData.push(addedProduct);
+        localStorage.setItem("basket", JSON.stringify(basketLSData));
+        alert("Product added Successfully");
+      }
+      document.querySelector("#beauty_product_container").append(div);
+      div.append(image_url, Name, dollar, Price,button);
+  
+  
+  
+    });
+  }
+  
+  
+  window.onscroll = function() {myFunction()};
+  
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+  
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+  
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+  
